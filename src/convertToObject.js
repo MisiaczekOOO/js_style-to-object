578415395fc3d546199afc3483f6fc6c03a78f16
Michał Mosór <mosormichal711@gmail.com>
2026-09-24 14:27:29 +0200

    Add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  // sourceString = sourceString.trim();
+
+  const temp = sourceString.split(';').map((word) => {
+    const [key, value] = word.split(':');
+
+    if (key && value) {
+      return [key.trim(), value.trim()];
+    }
+  });
+
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i] === undefined) {
+      continue;
+    }
+    result[temp[i][0]] = temp[i][1];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
